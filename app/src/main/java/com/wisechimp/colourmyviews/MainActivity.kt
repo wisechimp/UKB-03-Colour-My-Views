1d3173879d51b964e816ad58248a6169915cfd99
shicks1080 <stuart.hicks@gmail.com>
2019-08-26 17:43:24 +0200

    Update MainActivity.kt
    Making the boxes clickable

--- app/src/main/java/com/wisechimp/colourmyviews/MainActivity.kt
+++ app/src/main/java/com/wisechimp/colourmyviews/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.wisechimp.colourmyviews
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(box_one, box_two, box_three, box_four, box_five, background)
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColoured(it) }
+        }
+    }
+
+    private fun makeColoured(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
